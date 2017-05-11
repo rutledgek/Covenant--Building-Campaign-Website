a46b53bc2e8f376f2fd70b77105d5fae712190ec
rutledgek <kevin.m.rutledge@gmail.com>
2017-05-11 16:07:34 -0400

    Added ScrollReveal Jquery

---
+++ scripts/scrollreveal-site.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '100px',
+  easing: 'ease-out',
+  mobile: false,
+
+})
+
+sr.reveal('.right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '100px',
+  easing: 'ease-out',
+  mobile: false,
+
+})
