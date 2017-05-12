dec3ea06e95f9f7106a13ece95ed9ea32fa954c4
rutledgek <kevin.m.rutledge@gmail.com>
2017-05-12 01:50:57 -0400

    Progress on header with js opacity.

--- scripts/header.js
+++ scripts/header.js
@@ -1,12 +1,10 @@
-$(window).scroll(function(){
-
-    $('#main').each(function(){
-        var $this = $(this);
-        var el = $this.get(0);
-        var ele = $(".whatiftop");
-        if (isElementInViewport(el)) {
-            $ele.toggleclass('hide')
-        }
-    })
-
-})
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    
+     //>=, not <=
+    if (scroll >= 200) {
+        //clearHeader, not clearheader - caps H
+        $(".whatiftop").css("opacity", "100");
+    } else {
+      $(".whatiftop").css("opacity","0");}
+});
--- scripts/scrollreveal-site.js
+++ scripts/scrollreveal-site.js
@@ -1,6 +1,15 @@
 window.sr = ScrollReveal();
 
+sr.reveal('#doubletitle', {
+  duration: 1000,
+  origin: 'top',
+  // distance: '100px',
+  easing: 'ease-out',
+  mobile: false,
+})
+
 sr.reveal('.left', {
+  delay: 220,
   duration: 2000,
   origin: 'left',
   distance: '100px',
